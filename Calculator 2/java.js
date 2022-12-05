eb687a322e47927d745736f022e23e50348e416f
Eryka34 <115659511+Eryka34@users.noreply.github.com>
2022-12-05 13:50:34 -0500

    cal2

---
+++ Calculator 2/java.js
@@ -0,0 +1,31 @@
+
+//Store Result in Global Varible
+const result = document.getElementById("answer");
+
+//Declare function 
+function resultAssign () {
+    num = 3 + 5 + 50 + 500 + 87 + 2;
+    result.innerHTML = num;
+    console.log("Variable num represents the number: " + num);
+}
+//Call the function
+// resultAssign()
+
+
+//Function to Add Two and Two
+//Declare
+function addTwoPlusTwo() {
+    result.innerHTML = 2 + 2;
+}
+
+function addOnePlusSix() {
+    result.innerHTML = 1 + 6;
+}
+
+function addThreePlusFive() {
+    result.innerHTML = 3 + 5;
+}
+
+function addNinePlusTwo() {
+    result.innerHTML = 9 + 2;
+}
