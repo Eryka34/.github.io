d3e7cd33e686ddd8b8d83eb2a97d43731754fe15
Eryka34 <115659511+Eryka34@users.noreply.github.com>
2022-12-06 14:20:09 -0500

    light

--- Assignments/105_Lightbox/js/app.js
+++ Assignments/105_Lightbox/js/app.js
@@ -14,7 +14,18 @@ function openLightbox() {
 
 let coffeeImage = document.getElementById('coffee_L1');
 //When you click image, reference openLightbox function 
+
 coffeeImage.onclick = openLightbox;
+coffee_L2.onclick = openLightbox;
+coffee_L3.onclick = openLightbox;
+coffee_L4.onclick = openLightbox;
+coffee_P1.onclick = openLightbox;
+coffee_P2.onclick = openLightbox;
+coffee_P3.onclick = openLightbox;
+coffee_P4.onclick = openLightbox;
+coffee_S1.onclick = openLightbox;
+coffee_S2.onclick = openLightbox;
+coffee_S3.onclick = openLightbox;
 
 //Make Function to make closer clickable
 function closeLightbox() {
