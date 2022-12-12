f089f6d9c022d64d08aa0d7667cbda8e2c3b28d0
Eryka34 <115659511+Eryka34@users.noreply.github.com>
2022-12-12 10:33:19 -0500

    cal

---
+++ GSAP1/js/gsap.js
@@ -0,0 +1,54 @@
+// Square Animation
+gsap.from(".chucked", {
+    duration: 1,
+    rotation: 720,
+    ease: "bounce",
+    stagger: 0.5,
+    x: -500,
+    y: -500,
+});
+
+gsap.from(".chucked2", {
+    duration: 1,
+    rotation: 720,
+    ease: "bounce",
+    stagger: 0.5,
+    x: -500,
+    y: -500,
+});
+
+gsap.from(".chucked3", {
+    duration: 1,
+    rotation: 720,
+    ease: "bounce",
+    stagger: 0.5,
+    x: -500,
+    y: -500,
+});
+
+gsap.from(".chucked4", {
+    duration: 1,
+    rotation: 720,
+    ease: "bounce",
+    stagger: 0.5,
+    x: -500,
+    y: -500,
+});
+
+gsap.from(".chucked5", {
+    duration: 1,
+    rotation: 720,
+    ease: "bounce",
+    stagger: 0.5,
+    x: -500,
+    y: -500,
+});
+
+gsap.from(".chucked6", {
+    duration: 1,
+    rotation: 720,
+    ease: "bounce",
+    stagger: 0.5,
+    x: -500,
+    y: -500,
+});
